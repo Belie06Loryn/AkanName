123deba8ed1b181a103e9cc0f980648a5a58979d
Belie06Loryn <maniralexie@gmail.com>
2019-07-20 17:44:17 +0200

    adding arrays

--- days.js
+++ days.js
@@ -1,15 +1,19 @@
-<script>
-function gender() {
-  var male = document.getElementById("male");
-  var female = document.getElementById("female");
-  var text = document.getElementById("text");
-  if (male.checked == true){
-    male.style.display = "male";
-  } else if(female.checked == true)){
-    female.style.display = "female";
+// male akan name Array
+var men =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+// female akan name Array
+var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+// days of Week Array
+var days =["Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"];
+// months Array
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+  function gender(v){
+    var MM = parseFloat(days.bday.mm.value);
+  var DD = parseFloat(days.bday.dd.value);
+  var YY = parseFloat(days.bday.yy.value);
+  var CC = parseFloat(yy-1)/100+1;
+  var day =(((cc/4)-2*cc-1) + ((5*yy/4))+((26*(mm+1)/10))+ dd) % 7;
+  alert(Math.round(day));
+  days.da.value = da[day]
+  //..value = bday[MM/DD/YY]
   }
-  else{
-    text.style.display = "block";
- }
-}
-</script>
